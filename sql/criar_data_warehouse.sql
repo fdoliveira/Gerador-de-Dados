8cd2ac03ec5bd29e7e8821d43333a9eefd006e34
nobertojefferson78 <nobertojefferson78@gmail.com>
2016-12-06 15:27:45 -0200

    Atualização do arquivo na criação do dw

--- sql/criar_data_warehouse.sql
+++ sql/criar_data_warehouse.sql
@@ -1,35 +1,43 @@
-DROP DATABASE IF EXISTS topicos_especiais_data_warehouse;
-CREATE DATABASE topicos_especiais_data_warehouse;
-USE topicos_especiais_data_warehouse;
+DROP DATABASE IF EXISTS dw;
+CREATE DATABASE dw;
 
-create table local(
-	id varchar(202),
-	estado char(2),
-	cidade varchar(100),
-	bairro varchar(100),
-	unique(estado,cidade,bairro),
+CREATE TABLE IF NOT EXISTS dw.dm_local (
+ id	     			INT	 NOT NULL,
+ CodMunicipioGeo   	INT	 NOT NULL,
+ NomeMunicipioGeo	VARCHAR(60) NOT NULL,
+ CodMicroGeo    	INT	 NOT NULL,
+ NomeMicroGeo  	 	VARCHAR(60) NOT NULL,
+ CodMesoGeo    	 	INT      NOT NULL,
+ NomeMesoGeo    	VARCHAR(60) NOT NULL,
+ CodUFGeo	   		INT	 NOT NULL,	
+ SiglaUFGeo	 		CHAR(2)	 NOT NULL,	
+ NomeUFGeo	   		VARCHAR(20) NOT NULL,
+ CodRegiaoGeo	 	INT  NOT NULL,
+ RegiaoGeo	   		VARCHAR(15) NOT NULL,
+ PRIMARY KEY  (id)
+);
+
+create table dw.dm_franquia(
+	id int not null,
 	primary key(id)
 );
 
-create table franquia(
-	cod char(8) not null,
-	primary key(cod)
-);
-
-create table produto(
-	cod char(8) not null,
+create table dw.dm_produto(
+	id int not null,
 	nome_produto varchar(100),
-	primary key(cod)
+	primary key(id)
 );
 
-create table vendedor(
-	cpf char(11) not null,
+create table dw.dm_vendedor(
+	id int not null,
+    cpf varchar(13) not null,
 	nome varchar(250),
-	primary key(cpf)
+	primary key(id)
 );
 
-create table tempo(
-    id date not null,
+create table dw.dm_tempo(
+    id int not null,
+    tempo date,
 	ano int,
 	semestre int,
 	trimestre int,
@@ -38,20 +46,19 @@ create table tempo(
 	primary key(id)
 );
 
-create table vendas(
-	id int(11) not null auto_increment,
-	id_local varchar(202),
-	id_franquia char(8),
-	id_produto  char(8) ,
-	id_vendedor char(11),
-	id_tempo date,
+create table dw.ft_vendas(
+	id int not null auto_increment,
+	id_local int,
+	id_franquia int,
+	id_produto  int,
+	id_vendedor int,
+	id_tempo int,
 	qtd_produto int,
 	valor_total double,
-	foreign key(id_local) references local(id),
-	foreign key(id_franquia) references franquia(cod),
-	foreign key(id_produto) references produto(cod),
-	foreign key(id_vendedor) references vendedor(cpf),
-	foreign key(id_tempo) references tempo(id),
+	foreign key(id_local) references dm_local(id),
+	foreign key(id_franquia) references dm_franquia(id),
+	foreign key(id_produto) references dm_produto(id),
+	foreign key(id_vendedor) references dm_vendedor(id),
+	foreign key(id_tempo) references dm_tempo(id),
 	primary key(id)
-		
-);
+    );
